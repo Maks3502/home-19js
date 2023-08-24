d3b541983da038a406c18596b4b5ec254979d06d
Maks3502 <107855225+Maks3502@users.noreply.github.com>
2023-08-24 17:35:11 +0300

    Add files via upload

---
+++ home-19.js
@@ -0,0 +1,39 @@
+// завдання 1
+
+
+
+
+
+// const array2 = ['apple', 'banana', 'orange', 'banana', 'grape', 'apple', 'kiwi'];
+
+// const first = array2.map(element => element[0]);
+// console.log(first)
+
+// const last = array2.map(element => element[element.length - 1])
+// console.log(last)
+
+
+
+
+// завдання 2
+
+
+// const people = [
+//     { name: 'John', surname: "Doe", age: 32, occupation: 'programmer' },
+//     { name: 'Jane', surname: "Lee", age: 26, occupation: 'teacher' },
+//     { name: 'Mike', surname: "Watson", age: 42, occupation: 'engineer' },
+//     { name: 'Emily', surname: "Brad", age: 29, occupation: 'designer' }
+//     ];
+
+
+// const peopleName = people.map((user, index) => {
+
+//     return {
+// nameSurname : `${user.name} ${user.surname}`,
+        
+// id: index + 1,
+     
+//     }
+// })
+
+// console.log(peopleName)
